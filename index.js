eb08467e0d1edd554f1276574657ca21dba6d90f
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-13 00:05:14 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+
+  // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    message();
+  })
+function message(){
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
+  
+  
